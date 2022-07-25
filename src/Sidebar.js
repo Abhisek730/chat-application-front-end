fe74e7b26415041d2e9bc5d642a107a9e61f2b73
Abhishek73025 <abhi.ag.kas@gmal.com>
2022-07-25 12:38:41 +0530

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Sidebar.css";
+import man from "./img/man.png";
+import SidebarChat from "./SidebarChat";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/*--------------------------- Header------------------------- */}
+      <div className="sidebarHeader">
+        <div>
+          <img src={man} alt="" />
+        </div>
+        <div className="sidebarHeaderRight">
+          <button style={{ border: "none" }}>
+            <span className="material-symbols-outlined">
+              arrow_drop_down_circle
+            </span>
+          </button>
+          <button style={{ border: "none" }}>
+            <span className="material-symbols-outlined">more_vert</span>
+          </button>
+          <button style={{ border: "none" }}>
+            <span className="material-symbols-outlined">chat</span>
+          </button>
+        </div>
+      </div>
+      {/* --------------------------sidebar Search--------------------- */}
+      <div className="sidebarSearch">
+        <div className="sidebarSearchContainer">
+          <span className="material-symbols-outlined">search</span>
+          <input type="text" placeholder="Search contact" />
+        </div>
+      </div>
+      {/* ---------------------------Sidebar chats--------------------- */}
+      <div className="sidebarChats">
+        <SidebarChat addNewChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
+}
